82bc830e09ce608397016f4fe9d3fda9fd3f9705
Zach <z.d.jorgensen@gmail.com>
2022-09-17 22:07:44 -0600

    product columns set up

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +11,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      values: 10,
+      validate,
+    },
+    catetory_id: {
+      type: DataTypes.INTEGER,
+      references: Category.id,
+    },
   },
   {
     sequelize,
